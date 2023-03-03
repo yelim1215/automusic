de2afd72874d0b37252cf4675beff99f1c04f355
kimdanni <hyperreal_s2@naver.com>
2023-03-03 15:51:29 +0900

    🚀 UPDATE MUSIC GENERATION MODEL

---
+++ generate.py
@@ -0,0 +1,33 @@
+import argparse
+from typing import Dict
+
+from commu.midi_generator.generate_pipeline import MidiGenerationPipeline
+from commu.preprocessor.utils import constants
+
+
+def main():
+    pipeline = MidiGenerationPipeline(
+        {'checkpoint_dir': '/home/dani/workspace/checkpoint_best.pt'})
+
+    inference_cfg = pipeline.model_initialize_task.inference_cfg
+    model = pipeline.model_initialize_task.execute()
+
+    encoded_meta = pipeline.preprocess_task.execute({'output_dir': 'danni', 'bpm': 70, 'audio_key': 'aminor', 'time_signature': '4/4', 'pitch_range': 'mid_high', 'num_measures': 8.0, 'inst': 'acoustic_piano', 'genre': 'newage', 'track_role': 'main_melody', 'rhythm': 'standard',
+                                                    'min_velocity': 60, 'max_velocity': 80, 'chord_progression': 'Am-Am-Am-Am-Am-Am-Am-Am-G-G-G-G-G-G-G-G-F-F-F-F-F-F-F-F-E-E-E-E-E-E-E-E-Am-Am-Am-Am-Am-Am-Am-Am-G-G-G-G-G-G-G-G-F-F-F-F-F-F-F-F-E-E-E-E-E-E-E-E', 'num_generate': 3, 'top_k': 32, 'temperature': 0.95})
+    input_data = pipeline.preprocess_task.input_data
+
+    pipeline.inference_task(
+        model=model,
+        input_data=input_data,
+        inference_cfg=inference_cfg
+    )
+    sequences = pipeline.inference_task.execute(encoded_meta)
+
+    pipeline.postprocess_task(input_data=input_data)
+    pipeline.postprocess_task.execute(
+        sequences=sequences
+    )
+
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+from commu.preprocessor.utils import constants
+from commu.midi_generator.generate_pipeline import MidiGenerationPipeline
+from typing import Dict
+import argparse
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import APIKeyHeader
 import uuid
 from key import secrete_key
+
 
 app = FastAPI()
 api_key_header = APIKeyHeader(name="Token")
@@ -12,6 +17,28 @@ key : unique 한 key
 song : 노래 경로
 mood : list 형태로 저장
 """
+
+
+def main(model_arg: Dict, input_arg: Dict):
+    pipeline = MidiGenerationPipeline(model_arg)
+
+    inference_cfg = pipeline.model_initialize_task.inference_cfg
+    model = pipeline.model_initialize_task.execute()
+
+    encoded_meta = pipeline.preprocess_task.execute(input_arg)
+    input_data = pipeline.preprocess_task.input_data
+
+    pipeline.inference_task(
+        model=model,
+        input_data=input_data,
+        inference_cfg=inference_cfg
+    )
+    sequences = pipeline.inference_task.execute(encoded_meta)
+
+    pipeline.postprocess_task(input_data=input_data)
+    pipeline.postprocess_task.execute(
+        sequences=sequences
+    )
 
 
 class test_db:
@@ -29,8 +56,12 @@ async def root():
 
 
 @app.get("/auto_music")
-async def auto_music(token: str = Depends(api_key_header)):
-    print(secrete_key["SECRETE-KEY"])
+async def auto_music(control: int, token: str = Depends(api_key_header)):
+    # control must be : 00000000 (1/0, length : 8)
+    # main({'checkpoint_dir': '/home/dani/workspace/checkpoint_best.pt'}, {'output_dir': 'danni', 'bpm': 70, 'audio_key': 'aminor', 'time_signature': '4/4', 'pitch_range': 'mid_high', 'num_measures': 8.0, 'inst': 'acoustic_piano', 'genre': 'newage', 'track_role': 'main_melody', 'rhythm': 'standard',
+    #                                                                     'min_velocity': 60, 'max_velocity': 80, 'chord_progression': 'Am-Am-Am-Am-Am-Am-Am-Am-G-G-G-G-G-G-G-G-F-F-F-F-F-F-F-F-E-E-E-E-E-E-E-E-Am-Am-Am-Am-Am-Am-Am-Am-G-G-G-G-G-G-G-G-F-F-F-F-F-F-F-F-E-E-E-E-E-E-E-E', 'num_generate': 3, 'top_k': 32, 'temperature': 0.95})
     if token != secrete_key["SECRETE-KEY"]:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+    uid = uuid.uuid4()
+
     return {"hello": "world"}
